f091b97f87b3e60e46b4c8a3341ebad857f61636
zhouye39 <33239735+yexueer1998@users.noreply.github.com>
2018-01-03 11:28:10 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,75 @@
+# -*- coding:utf-8 -*-
+import urllib
+import urllib2
+import re
+ 
+#处理页面标签类
+class Tool:
+    #去除img标签,7位长空格
+    removeImg = re.compile('<img.*?>| {7}|')
+    #删除超链接标签
+    removeAddr = re.compile('<a.*?>|</a>')
+    #把换行的标签换为\n
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    #将表格制表<td>替换为\t
+    replaceTD= re.compile('<td>')
+    #把段落开头换为\n加空两格
+    replacePara = re.compile('<p.*?>')
+    #将换行符或双换行符替换为\n
+    replaceBR = re.compile('<br><br>|<br>')
+    #将其余标签剔除
+    removeExtraTag = re.compile('<.*?>')
+    def replace(self,x):
+        x = re.sub(self.removeImg,"",x)
+        x = re.sub(self.removeAddr,"",x)
+        x = re.sub(self.replaceLine,"\n",x)
+        x = re.sub(self.replaceTD,"\t",x)
+        x = re.sub(self.replacePara,"\n    ",x)
+        x = re.sub(self.replaceBR,"\n",x)
+        x = re.sub(self.removeExtraTag,"",x)
+        #strip()将前后多余内容删除
+        return x.strip()
+class BDTB:
+    def __init__(self,baseUrl,seeLZ,floorTag):
+        self.baseURL = baseUrl
+        self.seeLZ = '?see_lz='+str(seeLZ)
+        self.tool = Tool()
+        self.file = None
+        self.floor = 1
+        self.floorTag = floorTag
+    def getPage(self,pageNum):
+        try:
+            url = self.baseURL+ self.seeLZ + '&pn=' + str(pageNum)
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request)
+            return response.read().decode('utf-8')
+        except urllib2.URLError, e:
+            if hasattr(e,"reason"):
+                print u"连接百度贴吧失败,错误原因",e.reason
+                return None
+    def getTitle(self,page):
+        pattern = re.compile('<h1 class="core_title_txt.*?>(.*?)</h1>',re.S)
+        result = re.search(pattern,page)
+        if result:
+            return result.group(1).strip()
+        else:
+            return None
+    def getPageNum(self,page):
+        pattern = re.compile('<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>',re.S)
+        result = re.search(pattern,page)
+        if result:
+            return result.group(1).strip()
+        else:
+            return None
+    def getContent(self,page):
+        pattern = re.compile('<div id="post_content_.*?>(.*?)</div>',re.S)
+        items = re.findall(pattern,page)
+        floor = 1
+        for item in items:
+            print floor,u"楼————————————————\n"
+            print self.tool.replace(item)
+            floor += 1
+baseURL = 'http://tieba.baidu.com/p/5461670891' 
+seeLZ = raw_input("是否只获取楼主发言，是输入1，否输入0\n")
+floorTag = raw_input("是否写入楼层信息，是输入1，否输入0\n")
+bdtb = BDTB(baseURL,seeLZ,floorTag)
